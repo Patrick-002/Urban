eb742a3b126a60ad3ae17d32376bc559fab2260f
Kirill <kirilltymanov2000@gmail.com>
2024-07-17 19:49:34 +0300

    module_3_2 done

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    if '@' not in recipient or not (recipient[-4:] == '.com' or recipient[-3:] == '.ru' or recipient[-4:] == '.net') or\
+            '@' not in sender or not (sender[-4:] == '.com' or sender[-3:] == '.ru' or sender[-4:] == '.net'):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
