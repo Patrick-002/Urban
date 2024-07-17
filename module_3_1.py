ae87219cc30445947e2818f6ec1bfcfbae004996
Kirill <kirilltymanov2000@gmail.com>
2024-07-17 18:09:36 +0300

    module_3_1 done

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return {len(string), string.upper(), string.lower()}
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    found = False
+    for i in list_to_search:
+        if i.lower() == string.lower():
+            found = True
+            break
+    return found
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
