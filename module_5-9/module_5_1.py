ea17ac3149f723a1e8f2c3490800edc0fda42d71
Kirill <kirilltymanov2000@gmail.com>
2024-08-05 21:13:21 +0300

    module_5_1 done

---
+++ module_5-9/module_5_1.py
@@ -0,0 +1,17 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print("Такого этажа не существует")
+        else:
+            for i in range(new_floor):
+                print(i + 1)
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
