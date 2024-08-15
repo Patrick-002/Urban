714835e9d8d6154c49106fc640123d9eef12aacd
Kirill <kirilltymanov2000@gmail.com>
2024-08-15 18:50:27 +0300

    module_5_4 done

--- module_5-9/module_5_4.py
+++ module_5-9/module_5_4.py
@@ -1,7 +1,16 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
     def __len__(self):
         return self.number_of_floors
@@ -54,25 +63,14 @@ class House:
 
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+# Удаление объектов
+del h2
+del h3
 
-print(h1 == h2)  # __eq__
-
-h1 = h1 + 10  # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10  # __iadd__
-print(h1)
-
-h2 = 10 + h2  # __radd__
-print(h2)
-
-print(h1 > h2)  # __gt__
-print(h1 >= h2)  # __ge__
-print(h1 < h2)  # __lt__
-print(h1 <= h2)  # __le__
-print(h1 != h2)  # __ne__
+print(House.houses_history)
