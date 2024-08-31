55a9d3aced4c0606d40d22759a58f3d7c0a5420c
Kirill <kirilltymanov2000@gmail.com>
2024-08-31 16:18:02 +0300

    module_7_4 done

---
+++ module_5-9/module_7_4.py
@@ -0,0 +1,16 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+challenge_result = 'Победа команды Волшебники данных!'
+
+print("В команде Мастера кода участников: %s ! " % team1_num)
+print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
+print("Команда Волшебники данных решила задач: {} !".format(score_2))
+print(" Волшебники данных решили задачи за {} с !".format(team1_time))
+print(f"Команды решили {score_1} и {score_2} задач.")
+print(f"Результат битвы: {challenge_result}!")
