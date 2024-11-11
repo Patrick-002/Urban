e973499774f60f4ff6b663a8db7876a90d4642f9
Kirill <kirilltymanov2000@gmail.com>
2024-11-11 12:17:59 +0300

    module_13_3 done

---
+++ module_10-14/module_13_3.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.files import MemoryStorage
+import asyncio
+
+api = "7771128523:AAE6tlBg2GpJVrRc3hdhD157Kt8vCiAxVNg"
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
