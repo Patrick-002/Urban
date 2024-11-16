f571fba12a9f8d80c75aeb15c5ef001c0f2d0cf9
Kirill <kirilltymanov2000@gmail.com>
2024-11-17 00:52:50 +0300

    module_14_4 done

--- module_10-14/crud_functions.py
+++ module_10-14/crud_functions.py
@@ -10,7 +10,24 @@ def initiate_db():
     description TEXT,
     price INTEGER NOT NULL)
     ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL)
+    ''')
     connection.commit()
+
+def add_user(username, email, age):
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)', (username, email, age, 1000))
+
+def is_included(username):
+    user = cursor.execute('SELECT username FROM Users WHERE username = ?', (username,)).fetchone()
+    if user is None:
+        return False
+    else:
+        return True
 
 def get_all_products():
     cursor.execute('SELECT * FROM Products')
