e15f6d245cc2d7e6b1be248397092aace6c0b91b
Kirill <kirilltymanov2000@gmail.com>
2024-12-08 14:59:32 +0300

    module_16_3 done

---
+++ module_16-20/module_16_3.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/users")
+async def get_users() -> dict:
+    return users
+
+@app.post("/user/{username}/{age}")
+async def user_registration(username:str, age:str) -> str:
+    user_id = str(int(max(users, key=int))+1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def user_update(user_id: str, username: str, age:str) -> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} is updated"
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: str) -> str:
+    users.pop(user_id)
+    return f"The user {user_id} is deleted"
+
+#python -m uvicorn module_16_3:app
